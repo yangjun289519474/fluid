23e6720c7ca34d4b5ff94e509061a8e4f1c57457
zhanyi <hanson.yj@alibaba-inc.com>
2023-05-28 22:00:33 +0800

    update
    Signed-off-by: zhanyi <hanson.yj@alibaba-inc.com>

--- version.go
+++ version.go
@@ -18,7 +18,7 @@ type Version struct {
 }
 
 var (
-	version      = "0.0.0"                // value from VERSION file
+	version      = "0.0.1"                // value from VERSION file
 	buildDate    = "1970-01-01T00:00:00Z" // output from `date -u +'%Y-%m-%dT%H:%M:%SZ'`
 	gitCommit    = ""                     // output from `git rev-parse HEAD`
 	gitTag       = ""                     // output from `git describe --exact-match --tags HEAD` (if clean tree state)
